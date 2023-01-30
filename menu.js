ef4f7e285201bdb3463e637b5428e92acf3a23e1
Oluwatimilehin Esan <107879861+therealtmmy@users.noreply.github.com>
2023-01-30 22:13:03 +0100

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("menu-btn")
+const sushiBtn = document.getElementById("Sushi-btn")
+
+btn.addEventListener("click", function(){
+    sushiBtn.innerHTML += "<p>Work in Progress</p>"
+})
