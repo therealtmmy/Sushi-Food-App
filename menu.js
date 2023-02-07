8b3a5e07059fcd707109650cdbab61d1e984043c
Oluwatimilehin Esan <107879861+therealtmmy@users.noreply.github.com>
2023-02-07 23:49:40 +0100

    Delete menu.js

--- menu.js
+++
@@ -1,6 +0,0 @@
-const btn = document.getElementById("menu-btn")
-const sushiBtn = document.getElementById("Sushi-btn")
-
-btn.addEventListener("click", function(){
-    sushiBtn.innerHTML += "<p>Work in Progress</p>"
-})
