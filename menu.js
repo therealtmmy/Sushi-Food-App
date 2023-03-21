0dcac190ecfa0d4229d2ca3cc39e2e9fd9567bfe
therealtmmy <oluwatimilehinsamson@gmail.com>
2023-03-21 08:54:05 +0100

    Added API

--- menu.js
+++ menu.js
@@ -4,3 +4,16 @@ const sushiBtn = document.getElementById("Sushi-btn")
 btn.addEventListener("click", function(){
     sushiBtn.innerHTML += "<p>Work in Progress</p>"
 })
+
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php').then(data => {
+    return data.json(); 
+}).then(completedata =>{
+    console.log(completedata.categories);
+    let data1 = "";
+    completedata.categories.map((values)=>{
+        data1 +=`  
+        <div class="food"><img src="${values.strCategoryThumb}" alt="img"><figcaption>${values.strCategory}</figcaption></div>`
+    });
+    
+   document.getElementById("sushi").innerHTML = data1
+})
